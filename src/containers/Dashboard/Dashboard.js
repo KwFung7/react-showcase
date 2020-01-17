834f7f5040acc0088dbc7fe3c20a79eb1ac4f48f
KwFxng <kwfxng@gmail.com>
2020-01-17 10:33:07 +0800

    Dashboard Jumbotron update

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import { Button, Card, CardBody, Col, Jumbotron, Row } from 'reactstrap';
+import article from '../../article';
 
 class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    };
-  }
-
-  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
 
   render() {
-
     return (
       <div className="animated fadeIn">
-        TBC
+        <Row>
+          <Col>
+            <Card>
+              <CardBody>
+                <Jumbotron className="my-0">
+                  <h1 className="display-3 text-primary">Accenture React Showcase</h1>
+                  <p className="lead">Showcase aims to align the React coding style among Accenture's teams and provide coding example for new joiner.</p>
+                  <hr className="my-3" />
+                  <p className="lead">
+                    <a href={article.introduction} rel="noreferrer noopener" target="_blank">
+                      <Button color="primary">Discuss on Teams</Button>
+                    </a>
+                  </p>
+                </Jumbotron>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
