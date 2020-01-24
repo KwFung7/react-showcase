18fe1ecbcd835da000919f1040e7fdfd9db9a60f
Carla Fok <carla.c.fok@accenture.com>
2020-01-24 21:36:03 +0800

    Update tab component

--- src/components/BootstrapTabs/BootstrapTabs.js
+++ src/components/BootstrapTabs/BootstrapTabs.js
@@ -40,8 +40,7 @@ class BootstrapTabs extends React.Component {
         <TabContent activeTab={activeTab}>
           {tabs.map((tab, index) => (
             <TabPane tabId={tab.tabID}> 
-              <h4>Tab {index+1} Contents</h4>
-              <div>Tab {index+1} Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+              {tab.tabPaneBody}
             </TabPane>
           ))}
         </TabContent>
--- src/containers/Tabs/BasicTabs/BasicTabs.js
+++ src/containers/Tabs/BasicTabs/BasicTabs.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Card, CardBody, CardHeader } from 'reactstrap';
 import { AppSwitch } from '@coreui/react';
 import BootstrapTabs from '../../../components/BootstrapTabs';
@@ -8,10 +8,25 @@ class BasicTabs extends Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
+
+    const tabPaneBody1 = (
+      <Fragment>
+        <h4>Tab 1 Contents</h4>
+        <div>Tab 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+      </Fragment>
+    )
+      
+    const tabPaneBody2 = (
+      <Fragment>
+        <h4>Tab 2 Contents</h4>
+        <div>Tab 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+      </Fragment>
+    ) 
+
     this.tabs = [
-      { tabID: 'tab1', tabTitle: 'Tab 1', isDisabled: false},
-      { tabID: 'tab2', tabTitle: 'Tab 2', isDisabled: false},
-      { tabID: 'tab3', tabTitle: 'Tab 3', isDisabled: true}
+      { tabID: 'tab1', tabTitle: 'Tab 1', isDisabled: false, tabPaneBody: tabPaneBody1},
+      { tabID: 'tab2', tabTitle: 'Tab 2', isDisabled: false, tabPaneBody: tabPaneBody2},
+      { tabID: 'tab3', tabTitle: 'Tab 3', isDisabled: true, tabPaneBody: ''}
     ];
     this.state = {
       activeTab: 'tab1'
