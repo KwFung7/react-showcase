e4c8bf8e24e358c2132a82c746fcafcf48979521
Felix Kwan <kwfxng@gmail.com>
2020-02-02 19:08:59 +0800

    update carousel component

--- src/components/BootstrapCarousel/BootstrapCarousel.js
+++ src/components/BootstrapCarousel/BootstrapCarousel.js
@@ -4,53 +4,96 @@ import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCa
 
 const propTypes = {
   activeIndex: PropTypes.number,
-  next: PropTypes.func,
-  previous: PropTypes.func,
-  goToIndex: PropTypes.func,
-  setAnimating: PropTypes.func,
+  keyboard: PropTypes.bool,
+  interval: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+  mouseEnter: PropTypes.func,
+  mouseLeave: PropTypes.func,
+  slide: PropTypes.bool,
+  enableTouch: PropTypes.bool,
   items: PropTypes.array.isRequired
 };
 
-
 const defaultProps = {
   activeIndex: 0,
-  next: () => {},
-  previous: () => {},
-  goToIndex: () => {},
-  setAnimating: () => {},
-  items: []
+  keyboard: true,
+  interval: 5000,
+  mouseEnter: () => {},
+  mouseLeave: () => {},
+  slide: true,
+  enableTouch: true
 };
 
 class BootstrapCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: props.activeIndex,
+      animating: false
+    };
+  }
+
+  next = () => {
+    if (this.state.animating) return;
+    const nextIndex = this.state.activeIndex === this.props.items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({
+      activeIndex: nextIndex
+    });
+  };
+
+  previous = () => {
+    if (this.state.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? this.props.items.length - 1 : this.state.activeIndex - 1;
+    this.setState({
+      activeIndex: nextIndex
+    });
+  };
+
+  goToIndex = (newIndex) => {
+    if (this.state.animating) return;
+    this.setState({
+      activeIndex: newIndex
+    });
+  };
+
+  setAnimating = (animating) => {
+    this.setState({
+      animating: animating
+    });
+  };
+
+  slides = this.props.items.map((item, index) => {
+    return (
+      <CarouselItem
+        onExiting={() => this.setAnimating(true)}
+        onExited={() => this.setAnimating(false)}
+        key={index}
+      >
+        <img width="auto" src={item.src} alt={item.altText} />
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
 
   render() {
 
-    const { items, activeIndex, goToIndex, next, previous, setAnimating } = this.props;
-
-    const slides = items.map((item) => {
-      return (
-        <CarouselItem
-          onExiting={() => setAnimating(true)}
-          onExited={() => setAnimating(false)}
-          key={item.src}
-        >
-          <img width="auto" src={item.src} alt={item.altText} />
-          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
-        </CarouselItem>
-      );
-    });
+    const { items, activeIndex, ...otherProps } = this.props;
 
     return (
       <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-    >
-      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-      {slides}
-      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-    </Carousel>
+        activeIndex={this.state.activeIndex}
+        next={this.next}
+        previous={this.previous}
+        {...otherProps}
+      >
+        <CarouselIndicators items={items} activeIndex={this.state.activeIndex} onClickHandler={this.goToIndex} />
+        {this.slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
     )
   }
 }
